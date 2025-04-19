25cf95180d4939d821c0485dd03fa0fbd9c2be79
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-04-20 01:53:40 +0200

    add scanner

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,15 +1,21 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        String firstName = "Pinco";
-        String lastName = "Pallo";
-        String favoriteColor = "magenta";
-        byte dayOfBirth = 12;
-        byte monthOfBirth = 5;
-        short yearOfBirth = 1994;
+        Scanner input = new Scanner(System.in);
+
+        String firstName = input.nextLine();
+        String lastName = input.nextLine();
+        String favoriteColor = input.nextLine();
+        int dayOfBirth = input.nextInt();
+        int monthOfBirth = input.nextInt();
+        int yearOfBirth = input.nextInt();
         int sumBirthday = dayOfBirth + monthOfBirth + yearOfBirth;
 
         System.out.println(firstName + "-" + lastName + "-" + favoriteColor + "-" + sumBirthday);
+
+        input.close(); // close scanner
     }
 }
